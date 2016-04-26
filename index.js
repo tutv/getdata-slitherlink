e12fc1c8ce7b2aa16981847ba3623cbd29ac43f5
Tu TV <tutv95@gmail.com>
2016-04-26 18:03:35 +0700

    'init'

---
+++ index.js
@@ -0,0 +1,61 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var HtmlDom = require('htmldom');
+var fetchUrl = require('fetch').fetchUrl;
+
+var db = require('./nedb');
+
+app.get('/', function (req, res) {
+    res.json(db.types);
+});
+
+app.get('/:code', function (req, res) {
+    var code = req.params.code;
+    var size = db.types[code];
+
+    if (size === undefined) {
+        res.json([]);
+        return;
+    }
+
+    var url = 'http://www.puzzle-loop.com/?size=' + size;
+    fetchUrl(url, function (error, meta, body) {
+        var data = body.toString();
+
+        var html = new HtmlDom(data);
+        var $ = html.$;
+
+        var content = $('#LoopTable tr');
+
+        var arr = [];
+        for (var i = 0; i < content.length; i++) {
+            if (i % 2 != 0) {
+                var tr = content.eq(i);
+                var tds = tr.find('td');
+
+                for (var j = 0; j < tds.length; j++) {
+                    if (j % 2 != 0) {
+                        var td = tds.eq(j);
+                        var value = 0;
+                        if (td.html() != '') {
+                            value = parseInt(td.html());
+                        }
+
+                        arr.push(value);
+                    }
+                }
+            }
+        }
+
+        res.json(arr);
+    });
+});
+
+/**
+ * Server Listen
+ */
+http.listen(1235, function () {
+    console.log('listening on localhost:1235');
+});
---
+++ nedb.js
@@ -0,0 +1,22 @@
+var Datastore = require('nedb');
+
+var db = {};
+
+var types = {
+    '551': 0,
+    '552': 4,
+    '771': 10,
+    '772': 11,
+    '10101': 1,
+    '10102': 5,
+    '15151': 2,
+    '15152': 6,
+    '20201': 3,
+    '25301': 8,
+    '25302': 9
+};
+
+db.data = new Datastore({filename: 'databases/data.db', autoload: true});
+db.types = types;
+
+module.exports = db;
